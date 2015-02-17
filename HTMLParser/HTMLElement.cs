5640c44e6fb606dda72f710eecb786bc4ced2434
Jesus DF <jesusdf@gmail.com>
2015-02-17 11:33:37 +0100

    Create HTMLDocument class.

--- HTMLParser/HTMLElement.cs
+++ HTMLParser/HTMLElement.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace HTMLParser
 {
@@ -14,6 +16,7 @@ namespace HTMLParser
 		public string Value { get; set; }
 
 		public List<HTMLElement> Nodes { get; set; }
+
 	}
 }
 
