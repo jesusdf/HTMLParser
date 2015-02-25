f0f0cab3e1662ca46f2587c8cc161ecd64266dcb
Jesus DF <jesusdf@gmail.com>
2015-02-25 14:55:13 +0100

    Change class organization.

--- HTMLParser/HTMLElement.cs
+++ HTMLParser/HTMLElement.cs
@@ -7,14 +7,29 @@ namespace HTMLParser
 {
 	public class HTMLElement
 	{
-		public HTMLElement ()
-		{
+
+        private const int DEFAULT_COLUMNS = 12;
+
+		public HTMLElement() {
+            constructor(parent: null, width: 0);
 		}
+        public HTMLElement(HTMLElement parent) {
+            constructor(parent: parent, width: 0);
+        }
+        public HTMLElement(HTMLElement parent, int width) {
+            constructor(parent: parent, width: width);
+        }
+
+        public void constructor(HTMLElement parent, int width)
+        {
+            Parent = parent;
+            Width = width;
+        }
 
 		public string Tag { get; set; }
-
 		public string Value { get; set; }
-
+        public int Width { get; set; }
+        public HTMLElement Parent { get; set; }
 		public List<HTMLElement> Nodes { get; set; }
 
 	}
